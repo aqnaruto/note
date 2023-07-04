d45e8e66c584962811c39a3e1bf4b6692361cf76
n <n>
2023-07-04 17:37:04 +0800

    1

--- pw.js
+++ pw.js
@@ -1,6 +1,7 @@
 const pw = {
   x: "Xianglong18zhang",
   j: "jumpjump65416888",
+  j_s: "jumpjump",
   x_: "ianglong18zhang./",
 };
 
@@ -11,4 +12,12 @@ const browser = {
 
 const pay = {
   okk: ["18075930711", pw.x_],
+};
+
+const other = {
+  twitter: [
+    "@sm_ntr_/google_sso#liao1heng",
+    ["xiaoye_sekai", pw.j_s, "18075930711", "chrome中有保存"],
+  ],
+  telegram:'18075930711'
 };
