cedcf774ed5a5a737e5af33499e3ce0df35c9e57
n <n>
2023-07-04 16:16:11 +0800

    init

--- pw.js
+++ pw.js
@@ -1,4 +1,14 @@
+const pw = {
+  x: "Xianglong18zhang",
+  j: "jumpjump65416888",
+  x_: "ianglong18zhang./",
+};
 
-const pw ={
- 
-}
+const browser = {
+  chrome: ["liao1heng@gmail.com", "18075930711", pw.j],
+  firefox: ["co_uchihasasuke@outlook.com"],
+};
+
+const pay = {
+  okk: ["18075930711", pw.x_],
+};
